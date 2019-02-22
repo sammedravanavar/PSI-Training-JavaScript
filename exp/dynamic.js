06e0c973c6f52d96b78fb9218db0acba5ed1c331
Sammed Ravanavar <samravan@publicisgroupe.net>
2019-02-22 15:38:57 +0530

    not deleted

--- exp/dynamic.js
+++ exp/dynamic.js
@@ -2,19 +2,24 @@ var count = 0;
 function createPost() {
     var postContent = document.getElementById('myPost');
     var postContainer = document.getElementById('postContainer');
-    var myCard = `<div class="card mt-3" style="width: 18rem">
+    var myCard = `<div class="card mt-3 iCard" id="iCard" style="width: 18rem">
                     <img class="card-img-top" src="./`+count+`.png" alt="Card image cap">
                     <div class="card-body">
                         ${postContent.value}
                     </div>                
-                    <div style="float: right">
+                    <div style="text-align: right">
                         ${getTime()}
                     </div>
                   </div>`;
     postContainer.innerHTML=myCard+postContainer.innerHTML;
     postContent.value='';
-    count = (count+1)%3;
+    count = (count+1)%4;
     // createPost();
+    document.getElementsByClassName("iCard").addEventListener("dblclick",
+        function removeCard(){
+            for(var i=0; i<this.length; i++)
+            this[i].style.visibility =  "hidden";
+        })
 }
 function getTime(){
     var hour = new Date().getHours()
@@ -25,3 +30,10 @@ function getTime(){
     var time = date+"-"+month+"-"+year+"\t"+hour+":"+min
     return time
 }
+
+var btn = document.getElementById("btn")
+btn.addEventListener("click",createPost)
+// btn.addEventListener("dblclick",createPost)
+
+
+
